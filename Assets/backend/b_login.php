c5466c55f363275c6d4a4cb9558e81807ea09b67
Peagah <peagahvieira2003@gmail.com>
2022-09-02 08:05:44 -0300

    Pokehub

--- Assets/backend/b_login.php
+++ Assets/backend/b_login.php
@@ -5,6 +5,7 @@ session_start();
                 $conn = new PDO('mysql:host=localhost;dbname=pokehub', 'root', '');
                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
                 $username = $_POST['username'];  
                 $password = $_POST['password']; 
 
@@ -32,6 +33,31 @@ session_start();
                         'showMethod': 'fadeIn',
                         'hideMethod': 'fadeOut'
                     });</script>"; 
+                }
+                else if(isset($db_username['username']) == "$username" && isset($db_password['password']) == "$password"){
+                    $stm = $conn-> prepare('INSERT INTO user_login(username,password) VALUES (:username,:password)');
+                    $stm->bindParam('username', $username);
+                    $stm->bindParam('password', $password);
+                    $stm->execute();
+                    $_SESSION['username'] = $username;
+                    header("Refresh: 5;url=index.php");
+                    echo "<script type='text/javascript'>toastr.success('Login successful, redirecting in <b>5 seconds</b>', 'Situation', {
+                        'closeButton': true,
+                        'debug': false,
+                        'newestOnTop': false,
+                        'progressBar': true,
+                        'positionClass': 'toast-bottom-left',
+                        'preventDuplicates': false,
+                        'onclick': null,
+                        'showDuration': '300',
+                        'hideDuration': '1000',
+                        'timeOut': '5000',
+                        'extendedTimeOut': '1000',
+                        'showEasing': 'swing',
+                        'hideEasing': 'linear',
+                        'showMethod': 'fadeIn',
+                        'hideMethod': 'fadeOut'
+                    });</script>";
                 }
                 else if($_SESSION['count'] > 5){
                     echo "<script type='text/javascript'>toastr.warning('Suas tentativas acabaram, você será bloqueado!', 'Situação', {
@@ -64,7 +90,6 @@ session_start();
                             unset($_SESSION['end']);
                         }
                     }
-                    header("Refresh: 30;url=login.php");
                 }
                 else if(isset($db_username['username']) !== "$username" or isset($db_password['password']) !== "$password"){
                     echo "<script type='text/javascript'>toastr.error('Username or Password <b>incorrect</b>!', 'Situation', {
@@ -85,31 +110,6 @@ session_start();
                         'hideMethod': 'fadeOut'
                     });</script>";
                     $_SESSION['count'] ++;
-                }
-                else if(isset($db_username['username']) == "$username" && isset($db_password['password']) == "$password"){
-                    $stm = $conn-> prepare('INSERT INTO user_login(username,password) VALUES (:username,:password)');
-                    $stm->bindParam('username', $username);
-                    $stm->bindParam('password', $password);
-                    $stm->execute();
-                    $_SESSION['username'] = $username;
-                    header("Refresh: 5;url=index.php");
-                    echo "<script type='text/javascript'>toastr.success('Login successful, redirecting in <b>5 seconds</b>', 'Situation', {
-                        'closeButton': true,
-                        'debug': false,
-                        'newestOnTop': false,
-                        'progressBar': true,
-                        'positionClass': 'toast-bottom-left',
-                        'preventDuplicates': false,
-                        'onclick': null,
-                        'showDuration': '300',
-                        'hideDuration': '1000',
-                        'timeOut': '5000',
-                        'extendedTimeOut': '1000',
-                        'showEasing': 'swing',
-                        'hideEasing': 'linear',
-                        'showMethod': 'fadeIn',
-                        'hideMethod': 'fadeOut'
-                    });</script>";
                 }
             }
             catch(PDOException $e){
